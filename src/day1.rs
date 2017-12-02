07fff5c722b89ffedc14f070b600f772c53d3947
Kern <kern.walster@gmail.com>
2017-12-02 15:48:06 -0600

    Add solution for day 1 part 2 captcha challenge

--- src/day1.rs
+++ src/day1.rs
@@ -2,11 +2,21 @@
 /// Computes the captcha of an input slice
 /// where the captcha is defined as
 /// SUM a[i] where a[i] = a[i+1 % len(a)]
-pub fn captcha(numbers: &[u32]) -> u32 {
+fn captcha<F>(numbers: &[u32], step: F) -> u32   
+    where F: Fn(usize) -> usize  { 
     numbers.iter().enumerate()
-        .filter(|&(i,_)| numbers[i] == numbers[(i+1) % numbers.len()])
+        .filter(|&(i,_)| numbers[i] == numbers[step(i) % numbers.len()])
         .map(|(_, x)| x)
         .fold(0, |sum, x| sum + x)
+}
+
+pub fn simple_captcha(numbers: &[u32]) -> u32 {
+    captcha(numbers, |i| i + 1)
+}
+
+pub fn complex_captcha(numbers: &[u32]) -> u32 {
+    let len_numbers = numbers.len();
+    captcha(numbers, |i| i + len_numbers / 2)
 }
 
 #[cfg(test)]
@@ -14,31 +24,66 @@ mod test {
     use super::*;
 
     #[test]
-    fn test1() {
+    fn simple_test1() {
         let a = [1, 1, 2, 2];
-        let c = captcha(&a);
+        let c = simple_captcha(&a);
         assert_eq!(c, 3);
     }
 
     #[test]
-    fn test2() {
+    fn simple_test2() {
         let a = [1, 1, 1, 1];
-        let c = captcha(&a);
+        let c = simple_captcha(&a);
         assert_eq!(c, 4);
     }
 
     #[test]
-    fn test3() {
+    fn simple_test3() {
         let a = [1, 2, 3, 4];
-        let c = captcha(&a);
+        let c = simple_captcha(&a);
         assert_eq!(c, 0);
     }
 
     #[test]
-    fn test4() {
+    fn simple_test4() {
         let a = [9, 1, 2, 1, 2, 1, 2, 9];
-        let c = captcha(&a);
+        let c = simple_captcha(&a);
         assert_eq!(c, 9);
+    }
+
+    #[test]
+    fn complex_test1() {
+        let a = [1, 2, 1, 2];
+        let c = complex_captcha(&a);
+        assert_eq!(c, 6);
+    }
+
+    #[test]
+    fn complex_test2() {
+        let a = [1, 2, 2, 1];
+        let c = complex_captcha(&a);
+        assert_eq!(c, 0);
+    }
+
+    #[test]
+    fn complex_test3() {
+        let a = [1, 2, 3, 4, 2, 5];
+        let c = complex_captcha(&a);
+        assert_eq!(c, 4);
+    }
+
+    #[test]
+    fn complex_test4() {
+        let a = [1, 2, 3, 1, 2, 3];
+        let c = complex_captcha(&a);
+        assert_eq!(c, 12);
+    }
+
+    #[test]
+    fn complex_test5() {
+        let a = [1, 2, 1, 3, 1, 4, 1, 5];
+        let c = complex_captcha(&a);
+        assert_eq!(c, 4);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,20 @@ const USAGE: &'static str = "
 advent-2017
 
 Usage:
-  advent-2017 captcha <input>
+  advent-2017 captcha <variant> <input>
 
 ";
 
 #[derive(Debug, Deserialize)]
+enum Variant {
+    Simple,
+    Complex
+}
+
+#[derive(Debug, Deserialize)]
 struct Args {
     arg_input: String,
+    arg_variant: Variant,
     cmd_captcha: bool
 }
 
@@ -29,7 +36,10 @@ fn main() {
     if args.cmd_captcha {
         let input = util::string_to_number_slice(&args.arg_input);
         if let Some(input) = input {
-            println!("{}", day1::captcha(&input[..]));
+            match args.arg_variant {
+                Variant::Simple => println!("{}", day1::simple_captcha(&input[..])),
+                Variant::Complex => println!("{}", day1::complex_captcha(&input[..]))
+            }
             return;
         }
         println!("input must be a string of digits")
